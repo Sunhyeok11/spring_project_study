33427212e42be422e581d9355e3a2b4b9dbd75dc
SunHyeok <cghhld@naver.com>
2024-02-27 23:54:42 +0900

    username확인

--- servlet/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -19,8 +19,5 @@ public class HelloServlet extends HttpServlet {
         System.out.println("response = " + response);
         String username = request.getParameter("username");
         System.out.println("username = " + username);
-        response.setContentType("text/plain");
-        response.setCharacterEncoding("utf-8");
-        response.getWriter().write("hello " + username);
     }
 }
